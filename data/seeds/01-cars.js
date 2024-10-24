0520885ef6af58123d08e05d9bec7918f8db0cb1
3GGYlol <xlj135@gmail.com>
2024-10-24 14:10:59 -0400

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+    {
+        vin: '2HGED6349LH506746',
+        make: 'toyota',
+        model: 'prius',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '3D73Y3CL6BG585460',
+        make: 'toyota',
+        model: 'camry',
+        mileage: 75000,
+        title: 'salvage'
+    },
+    {
+        vin: 'JH4DC4450RS002529',
+        make: 'ford',
+        model: 'focus',
+        mileage: 15000,
+    }
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
